2fff4a5e5c3beea7c542575ea3ccd81a3ddca85b
John Berthels <john.berthels@gmail.com>
2020-12-03 05:52:31 +0000

    Need dummy files to run tests

--- env.sh
+++ env.sh
@@ -1,3 +1,4 @@
 export PATH=/home/john/src/go/bin:$PATH
 export GOPATH=/home/john/dev/jbert/aoc2020
 alias go2="go tool go2go"
+alias go2test="cp src/dummy.go src/aoc/dummy.go; cp src/dummy.go src/aoc/dummy_test.go; go tool go2go test aoc"
